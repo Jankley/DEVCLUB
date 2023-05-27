3811f55398584741fe91d8aa1e505592b779d238
jankley <dev.jankley96@gmail.com>
2023-05-27 16:13:26 -0300

    novo projeto

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const convertButton = document.querySelector(".convert-botao")
+
+function convertValues() {
+ console.log("funcionou")
+}
+
+
+
+convertButton.addEventListener("click", convertValues )
+
